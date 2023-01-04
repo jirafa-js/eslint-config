1663a93030ba7abded18ac86ccdaac845d171ddb
zhangxz16 <zhangxz16@asiainfo.com>
2023-01-04 11:47:46 +0800

    feat: off  rule `no-non-null-asserted-optional-chain`

--- packages/typescript/index.js
+++ packages/typescript/index.js
@@ -122,6 +122,7 @@ module.exports = {
     '@typescript-eslint/ban-ts-ignore': 'off',
     '@typescript-eslint/no-empty-function': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-non-null-asserted-optional-chain': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/ban-types': 'off',
     '@typescript-eslint/no-namespace': 'off',
